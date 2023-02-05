f1688475de9dac607eaa7a3431fb896fd9942b24
i12molml <118754069+i12molml@users.noreply.github.com>
2023-02-05 17:20:00 +0100

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,4 +1,11 @@
 #include <cute.h>
+#include <iostream>
+#include "usuario.h"
+#include "curso.h"
+#include "manejadorCursos.h"
+#include "manejadorUsuarios.h"
+#include "requisitos.h"
+
 void addCurso(){
 	ManejadorCursos mCurso;
 	Curso curso1("C001", "Programación en C++", 50, "2022-05-10", "2022-08-20", "Lunes y Miércoles de 18:00 a 21:00");
